c8fc336378f957ed087a6d0503f9970fe9cd5a3e
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2023-08-28 20:59:05 +0700

    modal image rundown

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const images = document.querySelectorAll(".images img");
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modalImg");
-const modalTxt = document.querySelector(".modalTxt");
-const close = document.querySelector(".close");
+document.addEventListener("DOMContentLoaded", () => {
+  // Kode JavaScript Anda di sini
+  const images = document.querySelectorAll(".images img");
+  const modal = document.querySelector(".modal");
+  const modalImg = document.querySelector(".modalImg");
+  const modalTxt = document.querySelector(".modalTxt");
+  const close = document.querySelector(".close");
 
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    modalImg.src = image.src;
-    modalTxt.innerHTML = image.alt;
-    modal.classList.add("appear");
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      modalImg.src = image.src;
+      modalTxt.innerHTML = image.alt;
+      modal.classList.add("appear");
+    });
+  });
 
-    close.addEventListener("click", () => {
+  close.addEventListener("click", () => {
+    modal.classList.remove("appear");
+  });
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
       modal.classList.remove("appear");
-    });
-
-    modal.addEventListener("click", (event) => {
-      if (event.target === modal) {
-        modal.classList.remove("appear");
-      }
-    });
+    }
   });
 });
 
